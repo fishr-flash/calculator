3dea96a09c1f9c82803657145dbae2278de17ee8
fishr-flash <fishr.flash@gmail.com>
2020-04-12 21:11:33 +0300

    refactoring reducer in part of simple operations and push num buttons

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from '../App';
-import reducers from '../reducers';
+import reducers from '../reducers/index1';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
