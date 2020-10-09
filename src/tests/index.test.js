94f570d71a6de4db84a245a67c51dd8301896e79
fishr-flash <fishr.flash@gmail.com>
2020-10-09 18:48:54 +0300

    create jest checking method servantResult and change native method Array.prototype.flat on custom method flatDeep and check here

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -23,13 +23,13 @@ describe( "trial check jest on example check work servantResult method ", ()=>{
             , firstOperator: SIMPLE_PLUS
             , onDot: false
             , arrLogText: [ "123", "+", "321", "=" ]
-        }
+        };
 
         expect( sr( inData )).toStrictEqual( outData );
 
     });
 
-    test( "check work the flatDeep function ( substitute the flat function js )", () =>{
+    test( "check work the flatDeep function ( substitute the Array.prototype.flat function js )", () =>{
 
         const inData = [1, 2, [3, 4, [5, 6]]];
         expect( flatDeep( inData )).toStrictEqual(  [1, 2, 3, 4, 5, 6] );
@@ -50,4 +50,70 @@ describe( "trial check jest on example check work servantResult method ", ()=>{
     });
 
 
-})
+});
+
+describe( "trial many incoming data of servantResult ", ()=>{
+
+    test( "trial many incoming data of servantResult", ()=>{
+
+        const checkedData = [
+            { /// обычное сравнение
+                inData:{ displayText:"321"
+                    , firstNumber: 123
+                    , lastNumber: 321
+                    , mode: MODES.LAST_NUMBER
+                    , firstOperator: SIMPLE_PLUS
+                    , onDot: false
+                    , arrLogText: [ "123", "+" ]}
+                , outData:{
+                    displayText: "444"
+                    , firstNumber: 444
+                    , lastNumber: 321
+                    , mode: MODES.AFTER_RESULT
+                    , firstOperator: SIMPLE_PLUS
+                    , onDot: false
+                    , arrLogText: [ "123", "+", "321", "=" ]
+                }
+            }
+            , {
+                inData:{ displayText:"321"
+                    , firstNumber: 123
+                    , lastNumber: 321
+                    , mode: MODES.LAST_NUMBER
+                    , firstOperator: SIMPLE_PLUS
+                    , onDot: false
+                    , arrLogText: [ "123", "+" ]}
+                , outData:{
+                    displayText: "444"
+                    , firstNumber: 444
+                    , lastNumber: 321
+                    , mode: MODES.AFTER_RESULT
+                    , firstOperator: SIMPLE_PLUS
+                    , onDot: false
+                    , arrLogText: [ "123", "+", "321", "=" ]
+                }
+            }, {
+                inData:{ displayText:"321"
+                    , firstNumber: 123
+                    , lastNumber: 321
+                    , mode: MODES.LAST_NUMBER
+                    , firstOperator: SIMPLE_PLUS
+                    , onDot: false
+                    , arrLogText: [ "123", "+" ]}
+                , outData:{
+                    displayText: "444"
+                    , firstNumber: 444
+                    , lastNumber: 321
+                    , mode: MODES.AFTER_RESULT
+                    , firstOperator: SIMPLE_PLUS
+                    , onDot: false
+                    , arrLogText: [ "123", "+", "321", "=" ]
+                }
+            }
+        ];
+
+        checkedData.forEach(( v, i ) =>{
+           expect( servantResult( v.inData )).toStrictEqual( v.outData );
+        });
+    });
+});
