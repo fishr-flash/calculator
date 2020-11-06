f4402d25aa65014a6ca605dad9921e8cde852c60
fishr-flash <fishr.flash@gmail.com>
2020-11-06 06:53:34 +0300

    Output.js - the option of using the function useCallback

--- src/calculator/Output.js
+++ src/calculator/Output.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {connect} from "react-redux";
 import {formatDisplayText} from "../reducers/utils";
 import {BIG_LENGTH_LOG_TEXT, FONT_SIZE_OUTPUT_WINDOW, WIDTH_OUTPUT_WINDOW} from "../constants";
@@ -10,7 +10,28 @@ function Output( {
 
     const displayText = formatDisplayText( rawText );
     const outputBlock = React.createRef();
+    const [ rightOffset, setRightOffset ] = useState( 0 );
+    const [ onManualShift, setOnManualShift ] = useState( false  );
 
+    const clickArrow = ( dirRight, logPWidth, rightOffset )=>{
+        /////////////////////////////CONSOLE/////////////////////////////////////
+        ///TODO: Console log in the code "OUTPUT_JS" line 84
+        if( true ){
+            console.group( 'Console log in the code "OUTPUT_JS" line 84' );
+            console.info( 'this: ', dirRight );
+            console.info( 'rightOffset: ', rightOffset );
+            console.info( 'logPWidth: ', logPWidth );
+            console.info( 'this: ', this );
+            //console.table( this );
+            console.groupEnd();
+        }
+        /////////////////////////////END CONSOLE/////////////////////////////////
+        setRightOffset( rightOffset + ( dirRight ? 100 : -100 ) );
+        setOnManualShift( true );
+
+    };
+
+    const onClickArrow = useCallback( ( dirRight, logPWidth, rightOffset )=>clickArrow( dirRight, logPWidth, rightOffset ), [ ])
     useEffect( ()=>{
 
         /// for resize font of output displayText
@@ -38,58 +59,59 @@ function Output( {
         const logSpan = outputBlock.current.children[ 0 ].children[ 1 ].firstChild;
         const logP = outputBlock.current.children[ 0 ].children[ 1 ];
         const logPWidth = outputBlock.current.parentNode.offsetWidth - arrowLeft.offsetWidth - arrowRight.offsetWidth - 4;
-        logP.style.width = `${ logPWidth }px`;
 
+        arrowLeft.onclick = ( e )=>{
+
+            onClickArrow( 0, logPWidth, rightOffset );
+        };
+        
+        
+        /////////////////////////////CONSOLE/////////////////////////////////////
+            ///TODO: Console log in the code "OUTPUT_JS" line 60
+            if( true ){
+                console.group( 'Console log in the code "OUTPUT_JS" line 60' );
+                console.info( 'arrowLeft.onClick: ', arrowLeft.onClick );
+                console.info( 'this: ', this );
+                //console.table( this );
+                console.groupEnd();
+            }
+        /////////////////////////////END CONSOLE/////////////////////////////////
+        logP.style.width = `${ logPWidth }px`;
 
         if( logText.length >= BIG_LENGTH_LOG_TEXT ){
             arrowRight.style.visibility = 'visible';
             arrowLeft.style.visibility = 'visible';
 
-            logSpan.style.right = `${ logSpan.offsetWidth - logPWidth }px`;
+            if( onManualShift === false )setRightOffset( logSpan.offsetWidth - logPWidth );
+
+            //logSpan.style.right = `${ rightOffset }px`;
         } else {
             logSpan.style.right = '0px';
             logSpan.style.left = '0px';
             arrowRight.style.visibility = 'hidden';
             arrowLeft.style.visibility = 'hidden';
         }
+    },[ setRightOffset, outputBlock, logText, onManualShift, onClickArrow ]);
 
 
 
-        /////////////////////////////CONSOLE/////////////////////////////////////
-            ///TODO: Console log in the code "OUTPUT_JS" line 44
-            if( true ){
-                console.group( 'Console log in the code "OUTPUT_JS" line 44' );
-                console.info( 'leftArrow: ', arrowLeft );
-                console.info( 'leftArrow: ', arrowLeft.offsetWidth );
-                console.info( 'rightArrow: ', arrowRight );
-                console.info( 'logSpan: ', logSpan );
-                console.info( 'logSpan: ', logSpan.offsetWidth );
-                console.info( 'logP: ', logP );
-                console.info( 'logP: ', logP.getBoundingClientRect() );
-                console.info( 'logP: ', logP.offsetWidth );
-                console.info( 'outputBlock.current.parent.width: ', outputBlock.current.parentNode );
-                console.info( 'outputBlock.current.parent.width: ', outputBlock.current.parentNode.getBoundingClientRect() );
-                console.info( 'outputBlock.current.parent.width: ', outputBlock.current.parentNode.offsetWidth );
-                console.info( 'logPWidth: ', logPWidth );
-                console.info( 'this: ', this );
-                //console.table( this );
-                console.groupEnd();
-            }
-        /////////////////////////////END CONSOLE/////////////////////////////////
-    });
 
     return(
         <section className="full_width"  ref={ outputBlock }>
             <div className="full_width story_calc">
                 <button
-                        className="progress_arrows left_progress_arrow"/>
+                        className="progress_arrows left_progress_arrow"
+                        /*onClick={ ()=>clickArrow( 0 ) }*/
+                />
                 <p className={ 'progress_content' } >
-                    <span className={ 'span_log_text' } style={{ textAlign: 'left'}} >
+                    <span className={ 'span_log_text' } style={{ textAlign: 'left', right: rightOffset}} >
                         { logText }
                     </span>
                 </p>
                 <button
-                        className="progress_arrows right_progress_arrow" />
+                        className="progress_arrows right_progress_arrow"
+                        onClick={ ()=>clickArrow( 1 ) }
+                />
             </div>
             <div className="full_width viewer_panel" id="viewer">
                 <blockquote>
